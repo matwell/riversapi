6965c6c71ab1c9917f85c67489561f680917956d
matwell <marcus.atwell@gmail.com>
2023-10-27 05:29:10 +0000

    only show changed discounts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { parseHTML } from 'linkedom';
 import { Feed } from 'feed';
 import fs  from "fs"
 
+type Shoe = {
+	id: string;
+	name: string;
+	price: number;
+	originalPrice: number;
+}
+
 async function getShoes(size:number) : Promise<string>{
 	const api = `https://www.rivers.com.au/rivers-footwear-mens-footwear?prefn1=size&prefv1=${size}&srule=sorting-CategoryPosition-Rivers-First&start=0&sz=60`;
 	const response = await fetch(api);
@@ -13,8 +20,8 @@ async function getShoes(size:number) : Promise<string>{
 	return await response.text();
 }
 
-function parsePage(html:string) : string[] {
-	const shoes = [];
+function parsePage(html:string) : Shoe[] {
+	const shoes : Shoe[] = [];
 	const { document } = parseHTML(html);
 
 	const rows = document.querySelectorAll<HTMLElement>('.product');
@@ -22,8 +29,8 @@ function parsePage(html:string) : string[] {
 		shoes.push({
 			id: row.getAttribute("data-pid"),
 			name: row.querySelector<HTMLElement>('.pdp-link').innerText,
-			price: row.querySelector('.sales > .value')?.getAttribute("content"),
-			originalPrice: row.querySelector(".strike-through > .value")?.getAttribute("content")
+			price: Number(row.querySelector('.sales > .value')?.getAttribute("content")),
+			originalPrice: Number(row.querySelector(".strike-through > .value")?.getAttribute("content"))
 		});
 	}
 	return shoes;
@@ -51,23 +58,38 @@ function createFeed(pageJson) {
     return feed
 }
 
-function writeJsonToFile(inString:string[], outFile) {
-	fs.writeFile(outFile, JSON.stringify(inString), (err) => {
+function writeJsonToFile(inShoes:Shoe[], outFile) {
+	fs.writeFile(outFile, JSON.stringify(inShoes), (err) => {
 		console.log("File written")
 	})
 }
 
 
-function compareToPrevious(params:type) {
-	
+function compareToPrevious(previous: Shoe[], current:Shoe[]) {
+	return current.filter((currentShoe: Shoe) => {
+        return !previous.some((previousShoe: Shoe) => {
+            return currentShoe.id === previousShoe.id;
+        });
+    });
+}
+
+function filterToDiscounts(shoes:Shoe[]) {
+	return shoes.filter((shoe: Shoe) => {
+		return shoe.price < shoe.originalPrice
+	})
 }
 
 async function buildRss() {
 	const html : string = await getShoes(11);
-	const pageJson:string[] = parsePage(html);
-	writeJsonToFile(pageJson, "previous.json")
-	console.log(pageJson)
-    const feed = createFeed(pageJson);
+	const pageJson:Shoe[] = parsePage(html);
+	const discountedShoes = filterToDiscounts(pageJson)
+	const previousFile = fs.readFileSync("data/previous.json", 'utf8')
+	const changedObjects = compareToPrevious(JSON.parse(previousFile),discountedShoes)
+	console.log(changedObjects)
+	// writeJsonToFile(discountedShoes, "data/previous.json")
+
+    const feed = createFeed(changedObjects);
+
     // console.log(feed.rss2())
 }
 
